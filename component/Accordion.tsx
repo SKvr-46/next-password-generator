713205dc7fc0f0213d84f3e6abbb3ebbba7cb6d2
SKvr-46 <koeich4@gmail.com>
2023-05-07 00:28:54 +0900

    fix accordion

--- component/Accordion.tsx
+++ component/Accordion.tsx
@@ -7,19 +7,19 @@ export const Accordion = () => {
             <p className={styles.state}>各条件に対するパスワードに含まれる文字の候補について：</p>
             <details>
                 <summary>大文字＆小文字＆記号＆数字</summary>
-                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&"から選択</p>
+                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!?@#$%&"からランダムに選択</p>
             </details>
             <details>
                 <summary>大文字＆小文字＆数字</summary>
-                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"から選択</p>
+                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"からランダムに選択</p>
             </details>
             <details>
                 <summary>大文字＆小文字＆記号</summary>
-                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?@#$%&"から選択</p>
+                <p>"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!?@#$%&"からランダムに選択</p>
             </details>
             <details>
                 <summary>大文字＆小文字</summary>
-                <p> "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"から選択</p>
+                <p> "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"からランダムに選択</p>
             </details>
         </div>
     )
