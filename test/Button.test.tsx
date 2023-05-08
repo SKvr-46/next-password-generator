5ae0a7af9206e8e19257e94c0603fc8697a4e186
SKvr-46 <koeich4@gmail.com>
2023-05-08 13:50:30 +0900

    add test

--- test/Button.test.tsx
+++ test/Button.test.tsx
@@ -38,4 +38,13 @@ describe("Buttonコンポーネントのテスト", () => {
             screen.getByRole("button", {name: passwordType })
         ).toBeInTheDocument();
     })
+
+    test('handlePasswordTypeがクリック時に正確に呼び出される', () => {
+        const setCharactersType = jest.fn()
+        const setCount = jest.fn()
+        render(<Button passwordType="大文字＆小文字＆記号＆数字" setCharactersType={setCharactersType} setCount={setCount} />)
+        fireEvent.click(screen.getByRole("button",{name: "大文字＆小文字＆記号＆数字"}))
+        expect(setCharactersType).toHaveBeenCalledWith(1)
+    })
+
 });
--- test/Generator.test.tsx
+++ test/Generator.test.tsx
@@ -1,6 +1,19 @@
 import { Generator } from "@/component/Generator";
-import { screen, render } from '@testing-library/react';
+import { Button } from "@/component/Button";
+import { screen, render, fireEvent } from '@testing-library/react';
+import React from "react";
 
 describe("generatorコンポーネントのテスト", () => {
-    test("")
+    const size = 10;
+    const setCount = jest.fn();
+    const setCharactersType = jest.fn();
+    const passwordType = "大文字＆小文字＆記号＆数字"
+
+    test("ボタンが表示される", () => {
+        render(<Generator size={size}/>);
+
+        expect(
+            screen.getByText(passwordType)
+        ).toBeInTheDocument();
+    })
 })
